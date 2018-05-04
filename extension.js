c000360575fa934369847d3e7fdab5acf893bfbf
olback <git@olback.net>
2018-05-04 14:55:42 +0200

    progress

--- extension.js
+++ extension.js
@@ -1,17 +1,94 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/*
+ *  VS Code Serial Monitor 2
+ *  github.com/olback/vscode-serial
+ */
+
+'use strict';
+
 const vscode = require('vscode');
-const serial = require('serialport');
+const SerialPort = require('serialport');
+
+const baudRates = [
+    '110',
+    '150',
+    '300',
+    '1200',
+    '2400',
+    '4800',
+    '9600',
+    '19200',
+    '38400',
+    '57600',
+    '115200',
+    '230400',
+    '460800',
+    '921600'
+];
+
+let ports = [];
+let openConnections = [];
+
+function scanPorts(showInfoMsg = true) {
+
+    ports = [];
+
+    SerialPort.list((err, _ports) => {
+
+        if (err) return vscode.window.showErrorMessage(err);
+
+        for (let port of _ports) {
+            if (typeof port.vendorId !== 'undefined') {
+                ports.push(port.comName);
+            }
+        }
+
+        if (!showInfoMsg) return;
+
+        if (ports.length === 1) {
+            // return vscode.window.setStatusBarMessage(`Found ${ports.length} serail device`);
+            return vscode.window.showInformationMessage(`Found ${ports.length} serail device`);
+        } else {
+            // return vscode.window.setStatusBarMessage(`Found ${ports.length} serail devices`);
+            return vscode.window.showInformationMessage(`Found ${ports.length} serail devices`);
+        }
+
+    });
+
+}
+
+function openPort() {
+
+    let port = {
+        name: null,
+        baud: null
+    }
+
+    // port.con = new SerialPort(port.name, { baudRate: port.baud });
+
+    vscode.window.showQuickPick(ports, { placeHolder: 'Serial port to open' })
+        .then(name => {
+            port.name = name;
+            vscode.window.showQuickPick(baudRates, { placeHolder: 'Baudrate. Default: 9600' })
+                .then(rate => {
+                    port.baud = Number(rate) ? Number(rate) : 9600;
+                    port.con = new SerialPort(port.name, { baudRate: port.baud });
+                    port.output = vscode.window.createOutputChannel(port.name);
+                    console.log(port);
+                });
+
+        });
+
+}
 
 function activate(context) {
 
     console.log('vscode-serial active');
 
-    let disposable = vscode.commands.registerCommand('vscode-serial.openPort', function () {
-        vscode.window.showInformationMessage('Open Serial port');
-    });
+    context.subscriptions.push(
+        vscode.commands.registerCommand('vscode-serial.scanPorts', scanPorts),
+        vscode.commands.registerCommand('vscode-serial.openPort', openPort)
+    );
 
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
